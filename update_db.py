407b8f43af1bbe857a6d536267717d31b2f97ea8
Franziska von der Goltz <franziska@vdgoltz.eu>
2016-12-29 13:07:47 +0100

    removed debugging print statements

--- update_db.py
+++ update_db.py
@@ -26,8 +26,7 @@ def get_newest():
     all_channel_urls = get_channel_urls()
 
     for channel_url in all_channel_urls:
-        print channel_url
-        print "!!!! NEW PODCAST !!!!!"
+
         # get associated channel object
         channel = Channel.query.filter_by(channel_url=channel_url).one()
 
@@ -36,15 +35,10 @@ def get_newest():
         channel_id = channel.channel_id
 
         if etag:
-            print "Updating the DB based on ETAG"
-            print "etag: ", etag
-            print "channel id: ", channel_id
 
             newest_feed = feedparser.parse(channel_url, etag=etag, modified=modified)
 
             if newest_feed.status == 200:
-                print "newest status", newest_feed.status
-                print "newest etag", newest_feed.etag
 
                 load_db.load_podcasts(newest_feed, channel_id)
 
@@ -54,20 +48,14 @@ def get_newest():
 
                 db.session.commit()
 
-            else:
-                print "can't find etag"
-                print newest_feed.status
-
         elif modified:
-            print "updating the db based on MODIFIED"
-            print "modified at: ", modified
-            print "channel id", channel_id
 
             newest_feed = feedparser.parse(channel_url, etag=etag, modified=modified)
 
             # checking if there are any items in the returned feed
             if newest_feed.status == 200:
-                print "newest status", newest_feed.status
+
+                # loading db with newest feeds
                 load_db.load_podcasts(newest_feed, channel_id)
                 channel.channel_modified = newest_feed.modified
 
@@ -75,13 +63,6 @@ def get_newest():
                 channel.channel_modified = newest_feed.modified
 
                 db.session.commit()
-
-            else:
-                print "can't find modified"
-                print newest_feed.status
-
-        else:
-            print "no etag"   
 
 
 if __name__ == "__main__":
